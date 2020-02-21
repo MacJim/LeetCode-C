a7486f5e7d2bc10f318497a81d0c7be30d6b1a6a
Jim Shi <shiyilin2008@gmail.com>
2020-02-20 23:52:04 -0800

    49: Adds a better string hash solution.

--- 49 (needs improvement).cpp
+++ 49 (needs improvement).cpp
@@ -34,7 +34,7 @@ public:
 // MARK: - 2. Sort strings
 // Runtime: 272 ms, faster than 5.01% of C++ online submissions for Group Anagrams.
 // Memory Usage: 17.7 MB, less than 95.52% of C++ online submissions for Group Anagrams.
-class Solution {
+class Solution2 {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& inputStrings) {
         const auto inputStringsSize = inputStrings.size();
@@ -61,6 +61,58 @@ public:
                 const auto returnValueIndex = std::distance(returnValueSortedRepresentations.begin(), iterator);
                 returnValue.at(returnValueIndex).push_back(currentInput);
             }
+        }
+
+        return returnValue;
+    }
+};
+
+
+// MARK: - 3. Use string as character occurrance hash.
+// Runtime: 116 ms, faster than 11.46% of C++ online submissions for Group Anagrams.
+// Memory Usage: 27 MB, less than 5.97% of C++ online submissions for Group Anagrams.
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        const auto strsSize = strs.size();
+        if (strsSize == 0) {
+            return {};
+        }
+
+        auto hashes = std::unordered_map<std::string, std::vector<size_t>>();
+
+        for (size_t i = 0; i < strs.size(); i += 1) {
+            const auto& aStr = strs.at(i);
+
+            // 26 characters
+            auto occurrances = std::vector<int>(26, 0);
+            for (const char& c: aStr) {
+                const size_t j = c - 'a';
+                occurrances.at(j) += 1;
+            }
+
+            std::string hash = "";
+            for (const int& anOccurrance: occurrances) {
+                hash += std::to_string(anOccurrance);
+                hash += ".";
+            }
+            
+            auto it = hashes.find(hash);
+            if (it == hashes.end()) {
+                std::vector<size_t> pos = {i};
+                hashes.emplace(hash, std::move(pos));
+            } else {
+                it->second.push_back(i);
+            }
+        }
+
+        auto returnValue = std::vector<std::vector<std::string>>();
+        for (const auto& aPair: hashes) {
+            auto currentReturnValue = std::vector<std::string>();
+            for (const auto& i: aPair.second) {
+                currentReturnValue.push_back(strs.at(i));
+            }
+            returnValue.push_back(std::move(currentReturnValue));
         }
 
         return returnValue;
