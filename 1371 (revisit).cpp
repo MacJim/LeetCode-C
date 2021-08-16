b99f3757d2a03b80a7567b3a11ce82f176a1b6bf
Jim Shi <shiyilin2008@gmail.com>
2021-08-15 17:09:17 -0700

    1371: Greedy, log first appearance of all permutations. Revisit.

--- 1371 (revisit).cpp
+++ 1371 (revisit).cpp
@@ -7,15 +7,26 @@
 #include <vector>
 #include <string>
 #include <array>
+#include <unordered_map>
 
 #include "helpers/Operators.hpp"
 #include "helpers/terminal_format.h"
 
 
-#pragma mark - 1. Test each length in a rolling fashion
+#pragma mark - 1. Test each length in a rolling fashion (my own solution, hard to implement the indices)
+/*
+ * Assume a string of length 6: "abcdef"
+ *
+ * - Pass 1: Test length 6: Forward: "abcdef"
+ * - Pass 2: Test length 5: Backward: "bcdef", "abcde"
+ * - Pass 3: Test length 4: Forward: "abcd", "bcde", "cdef"
+ * - Pass 4: Test length 3: Backward: "fed", "cde", "bcd" (found, return)
+ *
+ * Inter- and intra-passes are done similarly to rolling hashes.
+ */
 // Runtime: 44 ms, faster than 98.09% of C++ online submissions for Find the Longest Substring Containing Vowels in Even Counts.
 // Memory Usage: 14.1 MB, less than 99.90% of C++ online submissions for Find the Longest Substring Containing Vowels in Even Counts.
-class Solution {
+class Solution1 {
 private:
     inline int getOccurrenceIndex(const char& c) {
         switch (c) {
@@ -128,6 +139,121 @@ public:
 };
 
 
+#pragma mark - 2. Find the first occurrence of each character
+// WRONG ANSWER because I didn't log all combinations of even/odd numbers.
+class Solution2 {
+private:
+    inline int getOccurrenceIndex(const char& c) {
+        switch (c) {
+            case 'a':
+                return 0;
+
+            case 'e':
+                return 1;
+
+            case 'i':
+                return 2;
+
+            case 'o':
+                return 3;
+
+            case 'u':
+                return 4;
+
+            default:
+                return -1;
+        }
+    }
+
+private:
+//    static constexpr std::array<int, 5> expectedOccurrences = {true, true, true, true, true};
+
+public:
+    int findTheLongestSubstring(const std::string& s) {
+        auto firstOccurrences = std::array<int, 5>({-1, -1, -1, -1, -1});
+        auto isEven = std::array<bool, 5>({true, true, true, true, true});
+
+        int returnValue = 0;
+        for (int i = 0; i < s.size(); i += 1) {
+            const int occurrenceIndex = getOccurrenceIndex(s[i]);
+            if (occurrenceIndex != -1) {
+                isEven[occurrenceIndex] = !isEven[occurrenceIndex];
+                if (firstOccurrences[occurrenceIndex] == -1) {
+                    firstOccurrences[occurrenceIndex] = i;
+                }
+            }
+
+            int start = 0;
+            for (int j = 0; j < 5; j += 1) {
+                if (!isEven[j]) {
+                    // Will need to find the first matching occurrence.
+                    start = std::max(start, firstOccurrences[j] + 1);
+                }
+            }
+
+            const int currentDistance = i - start + 1;
+            returnValue = std::max(returnValue, currentDistance);
+        }
+
+        return returnValue;
+    }
+};
+
+
+#pragma mark - 3. Find the first occurrence of each even/odd combination (revisit)
+// Runtime: 80 ms, faster than 67.87% of C++ online submissions for Find the Longest Substring Containing Vowels in Even Counts.
+// Memory Usage: 14.4 MB, less than 99.10% of C++ online submissions for Find the Longest Substring Containing Vowels in Even Counts.
+class Solution {
+private:
+    inline int getOccurrenceIndex(const char& c) {
+        switch (c) {
+            case 'a':
+                return 0;
+
+            case 'e':
+                return 1;
+
+            case 'i':
+                return 2;
+
+            case 'o':
+                return 3;
+
+            case 'u':
+                return 4;
+
+            default:
+                return -1;
+        }
+    }
+
+public:
+    int findTheLongestSubstring(const std::string& s) {
+        /// The first occurrence of all even/odd **permutations**.
+        auto firstOccurrences = std::unordered_map<int, int>({{0, -1}});
+        /// Use a single integer to log the even/odd occurrences of all vowels.
+        int isOdd = 0;
+
+        int returnValue = 0;
+        for (int i = 0; i < s.size(); i += 1) {
+            const int occurrenceIndex = getOccurrenceIndex(s[i]);
+            if (occurrenceIndex != -1) {
+                isOdd ^= (1 << occurrenceIndex);    // XOR
+                if (!firstOccurrences.count(isOdd)) {
+                    firstOccurrences[isOdd] = i;    // Log first position of this permutation.
+                }
+            }
+
+            const int currentDistance = i - firstOccurrences[isOdd];
+            returnValue = std::max(returnValue, currentDistance);
+        }
+
+        return returnValue;
+    }
+};
+
+
+
 void test(const std::string& s, const int expectedResult) {
     auto solutionInstance = Solution();
 
@@ -145,6 +271,9 @@ int main() {
     test("eleetminicoworoep", 13);
     test("leetcodeisgreat", 5);
     test("bcbcbc", 6);
+    test("abcdef", 3);
+    test("bcbcbcbcbcabcd", 10);
+    test("tyrwpvlifrgjghlcicyocusukhmjbkfkzsjhkdrtsztchhazhmcircxcauajyzlppedqyzkcqvffyeekjdwqtjegerxbyktzvrxwgfjnrfbwvhiycvoznriroroamkfipazunsabwlseseeiimsmftchpafqkquovuxhhkpvphwnkrtxuiuhbcyqulfqyzgjjwjrlfwwxotcdtqsmfeingsxyzbpvmwulmqfrxbqcziudixceytvvwcohmznmfkoetpgdntrndvjihmxragqosaauthigfjergijsyivozzfrlpndygsmgjzdzadsxarjvyxuecqlszjnqvlyqkadowoljrmkzxvspdummgraiutxxxqgotqnxwjwfotvqglqavmsnmktsxwxcpxhuujuanxueuymzifycytalizwnvrjeoipfoqbiqdxsnclcvoafqwfwcmuwitjgqghkiccwqvloqrxbfjuxwriltxhmrmfpzitkwhitwhvatmknyhzigcuxfsosxetioqfeyewoljymhdwgwvjcdhmkpdfbbztaygvbpwqxtokvidtwfdhmhpomyfhhjorsmgowikpsdgcbazapkmsjgmfyuezaamevrbsmiecoujabrbqebiydncgapuexivgvomkuiiuuhhbszsflntwruqblrnrgwrnvcwixtxycifdebgnbbucqpqldkberbovemywoaxqicizkcjbmbxikxeizmzdvjdnhqrgkkqzmspdeuoqrxswqrajxfglmqkdnlescbjzurknjklikxxqqaqdekxkzkscoipolxmcszbebqpsizhwsxklzulmjotkrqfaeivhsedfynxtbzdrviwdgicusqucczgufqnaslpwzjhgtphnovlrgz", 831);
 
     return 0;
 }
